e6da671b4fe29bf359a5e2cd5f50d9a4c2b37cb8
rob jahn <robjahn@gmail.com>
2020-11-06 15:58:17 -0500

    adjust to new image name dt-orders

--- buildpush.sh
+++ buildpush.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-GITHUB_ACCOUNT=$1
-IMAGE=$GITHUB_ACCOUNT/keptn-orders-order-service
-
 clear
 if [ $# -lt 1 ]
 then
-  echo "missing arguments. Expect ./buildpush.sh <registry>"
+  echo "missing arguments. Expect ./buildpush.sh <REPOSITORY> "
+  echo "example:   ./buildpush.sh dtdemos"
   exit 1
 fi
+
+IMAGE=dt-orders-order-service
+REPOSITORY=$1
+FULLIMAGE=$REPOSITORY/$IMAGE
 
 #./mvnw clean package
 ./mvnw clean package -Dmaven.test.skip=true
 
-docker build -t $IMAGE:1 . --build-arg APP_VERSION=1
-docker build -t $IMAGE:2 . --build-arg APP_VERSION=2
-docker build -t $IMAGE:3 . --build-arg APP_VERSION=3
+docker build -t $FULLIMAGE:1 . --build-arg APP_VERSION=1
+docker build -t $FULLIMAGE:2 . --build-arg APP_VERSION=2
+docker build -t $FULLIMAGE:3 . --build-arg APP_VERSION=3
 
-echo ""
-echo "Ready to push images?"
+echo "========================================================"
+echo "Ready to push images ?"
+echo "========================================================"
 read -rsp "Press ctrl-c to abort. Press any key to continue"
 
-docker push $IMAGE:1
-docker push $IMAGE:2
-docker push $IMAGE:3
+docker push $FULLIMAGE:1
+docker push $FULLIMAGE:2
+docker push $FULLIMAGE:3
